4a56d8e6f614f4c37f8a6b9877ad4ca365fabf3a
MDeamer <maayan.dreamer@gmail.com>
2018-08-13 00:55:11 +1000

    Restored main file . not tested

--- scratch.py
+++ scratch.py
@@ -1,51 +1,25 @@
-import threading
+from node import Cluster
 import time
 import logging
-import socket
 
-#logging.basicConfig(filename='central_spike.log', level=logging.INFO, format='%(asctime)s %(message)s',
-#                    datefmt='%d/%m/%Y %I:%M:%S %p')
+all_nodes = Cluster (3,0.01)
+all_nodes.start()
 
 
-class Cluster(threading.Thread):
-    def __init__(self, numNodes, refRate, MCAST_GRP = "192.168.1.255", MCAST_PORT = 4210):
-        threading.Thread.__init__(self)
-        self.numNodes = numNodes
-        self.refRate = refRate  #refRate in Hz -> to ms(s) while in the loop
-        self.MCAST_GRP = MCAST_GRP
-        self.MCAST_PORT = MCAST_PORT
-        self.dataSocket = bytearray([0] * (self.numNodes * 3)) #array or rgb node, x3 is for the rgb
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    #set a node to an RGB color
-    def setNodeColor(self, nodeNum, red, green, blue):
-        if (nodeNum < 0) or (nodeNum >= self.numNodes):
-            logging.critical(nodeNum + ' is invalid node number')
-            return
+def main():
+    i = 0
+    while True:
+        all_nodes.setNodeColor(0, i, 0, 0)
+        all_nodes.setNodeColor(1, 0, i, 0)
+        all_nodes.setNodeColor(2, 0, 0, i)
 
-        if (red < 0) or (red > 255) or (green < 0) or (green > 255) or (blue < 0) or (blue > 255):
-            logging.critical('Invalid color on node number ' + nodeNum + ' color could be only 0-255')
-            return
+        i = i+1
+        if i >= 255:
+            i=0
+        time.sleep(0.01)
 
-        self.dataSocket[nodeNum*3] = red;
-        self.dataSocket[(nodeNum*3)+1] = green;
-        self.dataSocket[(nodeNum*3)+2] = blue;
+if __name__ == "__main__":
+    main()
 
-    def run(self):
-        self.loopDataCluster()
 
-    def senddatasocket(self):
-        self.sock.sendto(self.dataSocket, (self.MCAST_GRP, self.MCAST_PORT))
-        print('sending data ' + str(self.dataSocket))
-
-    # loop thread
-    def loopDataCluster(self):
-        while True:
-            try:
-                logging.debug('sending data to nodes')
-                self.senddatasocket()
-                time.sleep(self.refRate)
-            except Exception as e:
-                logging.critical(str(e) + ' cannot send data to cluster')
-                time.sleep(self.refRate)
