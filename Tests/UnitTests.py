052461bf6f730c1e31b47ddb4ef196a0689b7394
ItamarAmith <itamaram@gmail.com>
2018-08-31 23:02:33 +1000

    More play with animations

---
+++ Tests/UnitTests.py
@@ -0,0 +1,31 @@
+import unittest
+import rotation_animation
+
+
+class Tests(unittest.TestCase):
+    def test_get_polars(self):
+        for p in rotation_animation.get_polars():
+            print(p)
+
+    def test_get_scaled_polars(self):
+        for p in rotation_animation.get_cartesian_pts(3500):
+            print(p)
+
+    def test_get_layout(self):
+        print(rotation_animation.get_layout(3500))
+
+    def test_the_rainbow(self):
+        rotation_animation.play_rainbow('192.168.1.44:7890')
+
+    def test_shift(self):
+        self.assertEqual([3, 1, 2], rotation_animation.shift([1, 2, 3]))
+
+    def test_shift_tuple(self):
+        self.assertEqual((3, 1, 2), rotation_animation.shift((1, 2, 3)))
+
+    def test_clockwise_rainbow(self):
+        rotation_animation.clockwise_rainbow('192.168.1.44:7890')
+
+
+if __name__ == '__main__':
+    unittest.main()
--- rotation_animation.py
+++ rotation_animation.py
@@ -1,70 +1,136 @@
 from __future__ import division
+
+import sys
 import time
 import math
-import sys
-
 import opc
-import color_utils
+import json
 
 
-client = opc.Client('192.168.0.8:7890')
-if client.can_connect():
-    print('    connected to %s' % IP_PORT)
-else:
-    # can't connect, but keep running in case the server appears later
-    print('    WARNING: could not connect to %s' % IP_PORT)
-print('')
+def _connect(address):
+    client = opc.Client(address)
+    if client.can_connect():
+        print('    connected to %s' % address)
+    else:
+        # can't connect, but keep running in case the server appears later
+        print('    WARNING: could not connect to %s' % address)
+    print('')
 
-# -------------------------------------------------------------------------------
-# send pixels
+    return client
 
-print('    sending pixels forever (control-c to exit)...')
-print('')
 
-fps = 60  # frames per second
-
-def infloop(counter, increment: 1):
+def inf_loop(counter, increment: 1):
     while True:
-        for it in range(0, counter, increment):
-            yield it
+        for i in range(0, counter, increment):
+            yield i
 
 
-pts = [
-    [0.00, 1.71, 0.43],
-    [0.98, 1.40, 0.43],
-    [1.69, 0.30, 0.43],
-    [1.10, -1.31, 0.43],
-    [0.00, -1.71, 0.43],
-    [-1.21, -1.21, 0.43],
-    [-1.48, 0.86, 0.43],
-    [0.00, 1.14, 0.29],
-    [-0.21, 1.26, 0.77],
-    [-0.42, 1.38, 0.29],
-    [0.66, 0.94, 0.29],
-    [1.13, 0.20, 0.29],
-    [1.13, 0.20, 0.69],
-    [0.73, -0.88, 0.29],
-    [0.97, -0.83, 0.77],
-    [1.21, -0.79, 0.29],
-    [0.00, -1.14, 0.29],
-    [-0.81, -0.81, 0.29],
-    [-1.04, -0.75, 0.77],
-    [-1.27, -0.68, 0.29],
-    [-0.99, 0.57, 0.29],
-    [-0.99, 0.57, 0.69],
-    [0.33, 0.47, 0.43],
-    [0.37, -0.44, 0.43],
-    [-0.49, 0.29, 0.43]
-]
+# angles
+_7th = 2 * math.pi / 7
 
-start_time = time.time()
-for i in infloop(360, 2):
-    pixels = []
-    for p in pts:
-        midx = math.sin(i * math.pi / 180)
-        midy = math.cos(i * math.pi / 180)
-        dist = math.sqrt((midx - p[0]) ** 2 + (midy - p[1]) ** 2)
-        val = dist * 255 / 3
-        pixels.append((val, 0, 128))
-    client.put_pixels(pixels, channel=0)
-    time.sleep(1 / fps)
+
+class Band(object):
+    inner = 2000
+    mid = 4000
+    outer = 6000
+
+
+# heights
+class Height(object):
+    bot_duo = 1010.11
+    top_duo = 2420.13
+    moon = 1494.45
+    bot_trio = 1010.11
+    top_trio = 2692.5
+
+
+def inner_moon(segment):
+    return [(Band.inner, segment * _7th, Height.moon)]
+
+
+def outer_moon(segment):
+    return [(Band.outer, segment * _7th, Height.moon)]
+
+
+def planet(segment):
+    return [(Band.mid, segment * _7th, Height.bot_duo)]
+
+
+def pyramid(segment):
+    return [((Band.inner + Band.mid) / 2, segment * _7th, Height.bot_trio),
+            (Band.mid, segment * _7th, Height.top_trio),
+            ((Band.mid + Band.outer) / 2, segment * _7th, Height.bot_trio)]
+
+
+def totem(segment):
+    return [(Band.mid, segment * _7th, Height.bot_duo),
+            (Band.mid, segment * _7th, Height.top_duo)]
+
+
+def get_polars():
+    return totem(0) + outer_moon(0) + \
+           inner_moon(1) + planet(1) + outer_moon(1) + \
+           pyramid(2) + outer_moon(2) + \
+           inner_moon(3) + totem(3) + outer_moon(3) + \
+           pyramid(4) + outer_moon(4) + \
+           planet(5) + outer_moon(5) + \
+           inner_moon(6) + pyramid(6) + outer_moon(6)
+
+
+def get_cartesian_pts(scale):
+    return [(x[0] * math.cos(x[1]) / scale, x[0] * math.sin(x[1]) / scale, x[2] / scale) for x in get_polars()]
+
+
+def get_layout(scale):
+    return json.dumps([{"point": [round(x[0], 2), round(x[1], 2), round(x[2], 2)]} for x in get_cartesian_pts(scale)])
+
+
+def main():
+    client = _connect(address)
+    start_time = time.time()
+    for i in inf_loop(360, 2):
+        pixels = []
+        for p in pts:
+            midx = math.sin(i * math.pi / 180)
+            midy = math.cos(i * math.pi / 180)
+            dist = math.sqrt((midx - p[0]) ** 2 + (midy - p[1]) ** 2)
+            val = dist * 255 / 3
+            pixels.append((val, 0, 128))
+        client.put_pixels(pixels, channel=0)
+        time.sleep(1 / fps)
+
+
+def shift(arr, count=1):
+    return arr[-count:] + arr[:-count]
+
+
+def rainbow_stream():
+    for i in range(3):
+        for j in range(255):
+            yield shift((255 - j, j, 0), i)
+
+
+def play_rainbow(address='192.168.0.8:7890', fps=100):
+    client = _connect(address)
+    bands = [math.floor(x[0] // 25) for x in get_polars()]
+    buffer = [x for x in rainbow_stream()]
+    while True:
+        pixels = [buffer[bands[x]] for x in range(len(bands))]
+        client.put_pixels(pixels)
+        buffer = shift(buffer)
+        time.sleep(1/fps)
+
+
+def clockwise_rainbow(address='192.168.0.8:7890', fps=100):
+    client = _connect(address)
+    buffer = [x for x in rainbow_stream()]
+    slices = [math.floor(x[1] * len(buffer) / (2 * math.pi)) for x in get_polars()]
+    while True:
+        pixels = [buffer[slices[x]] for x in range(len(slices))]
+        client.put_pixels(pixels)
+        buffer = shift(buffer)
+        time.sleep(1/fps)
+
+
+if __name__ == "__main__":
+    main()
